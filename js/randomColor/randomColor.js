c6d8945dffc7397a553d6d13eec06985eaf0c5fd
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-09-07 18:58:51 +0330

    style(random-color page): add responsive design for screens over 768px

--- js/randomColor/randomColor.js
+++ js/randomColor/randomColor.js
@@ -1,48 +1,56 @@
 const carouselItems = document.querySelectorAll(".carousel-item");
 const carousel = document.querySelector(".carousel");
-let currentIndex = 0;
-const prevButton = document.querySelector(".carouselPrev");
-const nextButton = document.querySelector(".carouselNext");
-const showSlide = (index) => {
-  for (let i = 0; i < carouselItems.length; i++) {
-    carouselItems[i].classList.remove("active");
-  }
-  carouselItems[index].classList.add("active");
-};
-showSlide(currentIndex);
-prevButton.addEventListener("click", () => {
-  currentIndex--;
-  if (currentIndex < 0) {
-    currentIndex = carouselItems.length - 1;
-  }
+window.addEventListener("resize", () => {
+  if(innerWidth < 768) {
+  let currentIndex = 0;
+  const prevButton = document.querySelector(".carouselPrev");
+  const nextButton = document.querySelector(".carouselNext");
+  const showSlide = (index) => {
+    for (let i = 0; i < carouselItems.length; i++) {
+      carouselItems[i].classList.remove("active");
+    }
+    carouselItems[index].classList.add("active");
+  };
   showSlide(currentIndex);
-});
+  prevButton.addEventListener("click", () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = carouselItems.length - 1;
+    }
+    showSlide(currentIndex);
+  });
 
-nextButton.addEventListener("click", () => {
-  currentIndex++;
-  if (currentIndex > carouselItems.length - 1) {
-    currentIndex = 0;
-  }
-  showSlide(currentIndex);
-});
+  nextButton.addEventListener("click", () => {
+    currentIndex++;
+    if (currentIndex > carouselItems.length - 1) {
+      currentIndex = 0;
+    }
+    showSlide(currentIndex);
+  });
 
-let sliderInterval = setInterval(() => {
-  currentIndex++;
-  if (currentIndex > carouselItems.length - 1) {
-    currentIndex = 0;
-  }
-  showSlide(currentIndex);
-}, 3000);
-
-carousel.addEventListener("mouseenter", () => {
-  clearInterval(sliderInterval);
-});
-carousel.addEventListener("mouseleave", () => {
-  sliderInterval = setInterval(() => {
+  let sliderInterval = setInterval(() => {
     currentIndex++;
     if (currentIndex > carouselItems.length - 1) {
       currentIndex = 0;
     }
     showSlide(currentIndex);
   }, 3000);
+
+  carousel.addEventListener("mouseenter", () => {
+    clearInterval(sliderInterval);
+  });
+  carousel.addEventListener("mouseleave", () => {
+    sliderInterval = setInterval(() => {
+      currentIndex++;
+      if (currentIndex > carouselItems.length - 1) {
+        currentIndex = 0;
+      }
+      showSlide(currentIndex);
+    }, 3000);
+  });
+  } else{
+        for (let i = 0; i < carouselItems.length; i++) {
+      carouselItems[i].classList.remove("active");
+    }
+  }
 });
