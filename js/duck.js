ec33b6bee9166b11b66f311e4ee13466af7aa2bb
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-09-08 14:06:47 +0330

    fix(duck): fix size issue for screens above 768px

--- js/duck.js
+++ js/duck.js
@@ -1,16 +1,18 @@
 const duck = document.querySelector(".duck");
 
 duck.addEventListener("click", () => {
-  const duckSqueakSound = new Audio("../src/Audio/duck-squeak-toy.mp3");
+  const duckSqueakSound = new Audio("../assets/sounds/duck-squeak-toy.mp3");
   duckSqueakSound.play();
 });
 
 floor.addEventListener("click", (e) => {
   if (body.classList.contains("on")) {
     if (e.clientX < duck.offsetLeft) {
-      duck.style.transform = "scaleX(1)";
+      duck.classList.remove(("moveRight"))
+      duck.classList.add(("moveLeft"))
     } else {
-      duck.style.transform = "scaleX(-1)";
+      duck.classList.remove(("moveLeft"))
+      duck.classList.add(("moveRight"))
     }
     duck.style.left = e.clientX - 20 + "px";
     duck.classList.remove("walkAnimation");
